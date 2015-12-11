da57716be46af32be91d7a1a7a4c01b85077dcb0
Khaled Hosny <khaledhosny@eglug.org>
2015-12-11 16:18:17 +0400

    Unused imports

--- tools/build.py
+++ tools/build.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python2
 # encoding: utf-8
 
-from __future__ import division
-
 import argparse
-import math
 from datetime import datetime
 from sortsmill import ffcompat as fontforge
 
